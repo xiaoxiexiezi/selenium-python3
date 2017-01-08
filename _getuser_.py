f2f7a8050e4b13962e3fd08176f7580ba14885ba
tcwaily <tcwaily@me.com>
2017-01-08 22:23:01 +0800

    优化print输出

--- _getuser_.py
+++ _getuser_.py
@@ -2,17 +2,17 @@ import linecache
 import random
 z = '/Users/tcw/qqq.txt'
 
-count = len(open(z,'rU').readlines())
+count = len(open(z,'rU').readlines()) #获取文件总行数
 print('总行数:' + str(count))
 
-qqq = random.randint(1,count)
+qqq = random.randint(1,count) #随机1-count之间的数
 
 print('随机到第' + '|' + str(qqq) + '|' + '行')
 
-count1 = linecache.getline(z,qqq)
+count1 = linecache.getline(z,qqq) #打开文件，并获取变量qqq行 赋值给count1
 
-count2 = count1[9:20]
+count2 = count1[9:20] #截取第9-20位
 
-print('随机到第' + '|' + str(qqq) + '|' + '的内容为'  + str(count1) )
+count3 = '随机到第' + '|' + str(qqq) + '|' + '的内容为' + '[[' + str(count1).strip() + ']]'
 
-print(count2)
+print(count3)
