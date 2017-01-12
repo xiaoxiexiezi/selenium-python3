ad40a0e4d3eab1432589437db48c74ba0f9b5839
tcwaily <tcwaily@me.com>
2017-01-12 23:10:57 +0800

    一些修改

--- accountandcard.py
+++ accountandcard.py
@@ -11,7 +11,7 @@ class Untitled(unittest.TestCase):
     time.sleep(2)
     driver.find_element_by_id("login").clear()
     driver.find_element_by_id("login").send_keys(count2)
-    time.sleep(2)
+    time.sleep(1)
 
     phonefile1 = "/Users/tcw/already_registered.txt"
     count3 = len(open(phonefile1,'rU',encoding='utf-8').readlines()) #获取文件总行数
@@ -26,34 +26,53 @@ class Untitled(unittest.TestCase):
     data2=driver.find_element_by_xpath("//li/b").text
     print(data1 + ":" + data2)
 
-
-    try:
-        assert1 = driver.find_element_by_xpath("//a[contains(@href, '/verify_guides/new?step=one')]").text
-        print('--------测试通过---------')
-        print('该用户邮箱'+ assert1 )
-        print('--------测试通过---------')
-      #  driver.quit()
-    except:
-        print("测试失败")
+    print('*************************')
     try:
         assert2 = driver.find_element_by_xpath('//p[3]/a').text
-        print('--------测试通过---------')
-        print('该用户身份' + assert2)
-        print('--------测试通过---------')
-    #    driver.quit()
+        print('--------------')
+        print('该用户身份' + assert2  )
+        print('--------------')
 
     except:
         print("测试失败")
+        driver.find_element_by_xpath('//a[3]/b').click()
 
-        driver.find_element_by_css_selector("b").click()
+    if
+    print('*************************')
 
     try: #检查手机号
         assert3 = driver.find_element_by_xpath('//p[3]/a[2]').text
-        print('手机号'+assert3)
+        print('----------------------------------')
+        print('该用户手机号'+assert3+'：手机号为'+str(count2))
+        print('----------------------------------')
     except:
-        print('123')
-    print('本次登录使用的手机号为|' + str(count2) + '|')
+        print()
+    print('*************************')
+    try:
+        assert1 = driver.find_element_by_xpath("//a[contains(@href, '/verify_guides/new?step=one')]").text
+        print('--------------')
+        print('该用户邮箱'+ assert1 )
+        print('--------------')
+      #  driver.quit()
+    except:
+        print("测试失败")
+    print('*************************')
 
+    try: #检查银行卡
+        assert4 = driver.find_element_by_xpath('//a[4]').text
+        print('----------------------------------')
+        print('该用户银行卡' + assert4)
+        print('----------------------------------')
+        driver.get('https://testv2.pandai.cn/users/50131/bank_accounts')
+        time.sleep(1)
+        bank = driver.find_element_by_css_selector('h2').text
+        banknum = driver.find_element_by_css_selector("div.afba-img-cen1 > p").text
+        print('该绑卡银行为：' + bank + '|' + '卡号为：' + banknum)
+        print(bank)
+    except:
+        print('有错误了')
+
+    driver.quit()
 
 if __name__ == "__main__":
     unittest.main()
